908b7a3381cc5304fd5a3f437ab6e01386239832
Kavin0M <kavinsai88@gmail.com>
2023-11-07 18:10:15 +0530

    Added features

--- app.js
+++ app.js
@@ -50,16 +50,20 @@ function printRandomData(data){
         }
     }
 
-    window.innerHTML = `<div class="food-card" onclick="document.getElementById('random-model').style.display='block'">
+    window.innerHTML = `<div class="food-card">
                     <img src="${data.strMealThumb}" alt="food-img">
+                    <div class="buttons">
+                    <div class="btn1" onclick="document.getElementById('random-ingredient').style.display='block'">Ingredients</div>
+                    <div class="btn2" onclick="document.getElementById('random-instruction').style.display='block'">Instructions</div>
+                    </div>
                     <div class="food-name">${data.strMeal}</div>
                 </div>
                 
-                <div id="random-model" class="w3-modal">
+                <div id="random-ingredient" class="w3-modal">
                     <div class="w3-modal-content w3-animate-top w3-poppins">
 
                         <header class="w3-container w3-teal">
-                            <span onclick="document.getElementById('random-model').style.display='none'"
+                            <span onclick="document.getElementById('random-ingredient').style.display='none'"
                             class="w3-button w3-display-topright">&times;</span>
                             <h2>${data.strMeal}</h2>
                         </header>
@@ -71,6 +75,22 @@ function printRandomData(data){
                             </tr>
                             ${tableData}
                         </table>
+                    </div>
+                </div>
+                
+                <div id="random-instruction" class="w3-modal">
+                    <div class="w3-modal-content">
+
+                        <header class="w3-container w3-teal">
+                          <span onclick="document.getElementById('random-instruction').style.display='none'"
+                          class="w3-button w3-display-topright">&times;</span>
+                          <h2>Instructions</h2>
+                        </header>
+
+                        <div class="w3-container w3-padding-16">
+                          ${data.strInstructions}
+                        </div>
+
                     </div>
                 </div>`
 }
@@ -96,16 +116,20 @@ function printSearchData(data){
                 }
             }
 
-            window.innerHTML += `<div class="food-card" onclick="document.getElementById('idno${index}').style.display='block'">
+            window.innerHTML += `<div class="food-card">
                     <img src="${item.strMealThumb}" alt="food-img">
+                    <div class="buttons">
+                    <div class="btn1" onclick="document.getElementById('ingredient${index}').style.display='block'">Ingredients</div>
+                    <div class="btn2" onclick="document.getElementById('instruction${index}').style.display='block'">Instructions</div>
+                    </div>
                     <div class="food-name">${item.strMeal}</div>
                 </div>
                 
-                <div id="idno${index}" class="w3-modal">
+                <div id="ingredient${index}" class="w3-modal">
                     <div class="w3-modal-content w3-animate-top w3-poppins">
 
                         <header class="w3-container w3-teal">
-                            <span onclick="document.getElementById('idno${index}').style.display='none'"
+                            <span onclick="document.getElementById('ingredient${index}').style.display='none'"
                             class="w3-button w3-display-topright">&times;</span>
                             <h2>${item.strMeal}</h2>
                         </header>
@@ -117,6 +141,22 @@ function printSearchData(data){
                             </tr>
                             ${tableData}
                         </table>
+                    </div>
+                </div>
+                
+                <div id="instruction${index}" class="w3-modal">
+                    <div class="w3-modal-content">
+
+                        <header class="w3-container w3-teal">
+                          <span onclick="document.getElementById('instruction${index}').style.display='none'"
+                          class="w3-button w3-display-topright">&times;</span>
+                          <h2>Instructions</h2>
+                        </header>
+
+                        <div class="w3-container w3-padding-16">
+                          ${res.data.meals[0].strInstructions}
+                        </div>
+
                     </div>
                 </div>`
         })
